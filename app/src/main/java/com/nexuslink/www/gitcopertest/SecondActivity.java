fec7e6b8a765b828f3332fab5114bca0b0a22826
zhoujiahao <zhou.jiahao@foxmail.com>
2017-01-13 22:56:24 +0800

    Second Commit

---
+++ app/src/main/java/com/nexuslink/www/gitcopertest/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.nexuslink.www.gitcopertest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ASUS-NB on 2017/1/13.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
